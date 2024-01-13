02a0cab665c32bd044e59f1a01b2d271e810fc69
TheNumbat <mslater@nevada.unr.edu>
2024-01-13 12:46:22 -0500

    fix ci

--- rvk/rvk.cpp
+++ rvk/rvk.cpp
@@ -1,2 +1,117 @@
 
 #include "rvk.h"
+
+namespace rvk {
+
+using namespace rpp;
+
+namespace impl {
+
+struct Vk {
+    Vk(Config config) {
+        static_cast<void>(config);
+    }
+};
+
+static Opt<Vk> singleton;
+
+void check(VkResult result) noexcept {
+    RVK_CHECK(result);
+}
+
+[[nodiscard]] String_View describe(VkResult result) noexcept {
+    switch(result) {
+#define STR(r)                                                                                     \
+    case VK_##r: return #r##_v
+        STR(SUCCESS);
+        STR(NOT_READY);
+        STR(TIMEOUT);
+        STR(EVENT_SET);
+        STR(EVENT_RESET);
+        STR(INCOMPLETE);
+        STR(ERROR_OUT_OF_HOST_MEMORY);
+        STR(ERROR_OUT_OF_DEVICE_MEMORY);
+        STR(ERROR_INITIALIZATION_FAILED);
+        STR(ERROR_DEVICE_LOST);
+        STR(ERROR_MEMORY_MAP_FAILED);
+        STR(ERROR_LAYER_NOT_PRESENT);
+        STR(ERROR_EXTENSION_NOT_PRESENT);
+        STR(ERROR_FEATURE_NOT_PRESENT);
+        STR(ERROR_INCOMPATIBLE_DRIVER);
+        STR(ERROR_TOO_MANY_OBJECTS);
+        STR(ERROR_FORMAT_NOT_SUPPORTED);
+        STR(ERROR_SURFACE_LOST_KHR);
+        STR(ERROR_NATIVE_WINDOW_IN_USE_KHR);
+        STR(SUBOPTIMAL_KHR);
+        STR(ERROR_OUT_OF_DATE_KHR);
+        STR(ERROR_INCOMPATIBLE_DISPLAY_KHR);
+        STR(ERROR_VALIDATION_FAILED_EXT);
+        STR(ERROR_INVALID_SHADER_NV);
+#undef STR
+    default: return "UNKNOWN_ERROR"_v;
+    }
+}
+
+[[nodiscard]] String_View describe(VkObjectType type) noexcept {
+    switch(type) {
+#define STR(r)                                                                                     \
+    case VK_OBJECT_TYPE_##r: return #r##_v
+        STR(UNKNOWN);
+        STR(INSTANCE);
+        STR(PHYSICAL_DEVICE);
+        STR(DEVICE);
+        STR(QUEUE);
+        STR(SEMAPHORE);
+        STR(COMMAND_BUFFER);
+        STR(FENCE);
+        STR(DEVICE_MEMORY);
+        STR(BUFFER);
+        STR(IMAGE);
+        STR(EVENT);
+        STR(QUERY_POOL);
+        STR(BUFFER_VIEW);
+        STR(IMAGE_VIEW);
+        STR(SHADER_MODULE);
+        STR(PIPELINE_CACHE);
+        STR(PIPELINE_LAYOUT);
+        STR(RENDER_PASS);
+        STR(PIPELINE);
+        STR(DESCRIPTOR_SET_LAYOUT);
+        STR(SAMPLER);
+        STR(DESCRIPTOR_POOL);
+        STR(DESCRIPTOR_SET);
+        STR(FRAMEBUFFER);
+        STR(COMMAND_POOL);
+        STR(SAMPLER_YCBCR_CONVERSION);
+        STR(DESCRIPTOR_UPDATE_TEMPLATE);
+        STR(SURFACE_KHR);
+        STR(SWAPCHAIN_KHR);
+        STR(DISPLAY_KHR);
+        STR(DISPLAY_MODE_KHR);
+        STR(DEBUG_REPORT_CALLBACK_EXT);
+        STR(DEBUG_UTILS_MESSENGER_EXT);
+        STR(VALIDATION_CACHE_EXT);
+#undef STR
+    default: return "UNKNOWN_OBJECT"_v;
+    }
+}
+
+} // namespace impl
+
+// API
+
+[[nodiscard]] bool startup(Config config) noexcept {
+    if(impl::singleton) {
+        die("[rvk] already started up!");
+    }
+    impl::singleton = impl::Vk{config};
+    info("[rvk] completed startup.");
+    return true;
+}
+
+void shutdown() noexcept {
+    impl::singleton = {};
+    info("[rvk] completed shutdown.");
+}
+
+} // namespace rvk
--- rvk/rvk.h
+++ rvk/rvk.h
@@ -1,2 +1,21 @@
 
 #pragma once
+
+#include <rpp/base.h>
+
+#include "fwd.h"
+
+namespace rvk {
+
+using namespace rpp;
+
+struct Config {
+    u32 frames_in_flight = 2;
+    bool use_validation = true;
+    Slice<String_View> instance_extensions;
+};
+
+[[nodiscard]] bool startup(Config config) noexcept;
+void shutdown() noexcept;
+
+} // namespace rvk
